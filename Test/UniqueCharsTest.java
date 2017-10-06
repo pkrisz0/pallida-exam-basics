b0ddc0bea2ae66ed2a94e46bebe5b9694f440624
pkrisz0 <p.krisz89@gmail.com>
2017-10-06 10:05:41 +0200

    second test implemented

---
+++ Test/UniqueCharsTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniqueCharsTest {
+
+    UniqueChars test = new UniqueChars();
+
+    @Test
+    void uniqueCharacters() {
+        assertEquals(Arrays.asList('n','g','r','m'), test.uniqueCharacters("anagram"));
+    }
+
+    @Test
+    void uniqueCharacters_2() {
+        assertEquals(Arrays.asList('0'), test.uniqueCharacters(""));
+    }
+
+    @Test
+    void uniqueCharacters_3() {
+        assertEquals(Arrays.asList('0'), test.uniqueCharacters(""));
+    }
+
+}
