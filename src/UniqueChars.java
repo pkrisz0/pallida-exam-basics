44024d4747f68628db9948ddebd003f0beb1ca67
pkrisz0 <p.krisz89@gmail.com>
2017-10-06 09:56:55 +0200

    first test complete

--- src/UniqueChars.java
+++ src/UniqueChars.java
@@ -3,18 +3,11 @@ import java.util.List;
 
 public class UniqueChars {
     public static void main(String[] args) {
-        // Create a function called `uniqueCharacters` that takes a string as parameter
-        // and returns a list with the unique letters of the given string
-        // Create basic unit tests for it with at least 3 different test cases
         System.out.println(uniqueCharacters("anagram"));
-        // Should print out:
-        // ["n", "g", "r", "m"]
     }
 
     public static List uniqueCharacters(String input){
         List<Character> unique = new ArrayList<>();
-
-
 
         for (int i = 0; i < input.length(); i++) {
             int frequency = input.length() - (input.replaceAll(String.valueOf(input.charAt(i)), "")).length();
@@ -22,7 +15,13 @@ public class UniqueChars {
                 unique.add(input.charAt(i));
             }
         }
-
         return unique;
     }
 }
+
+// Create a function called `uniqueCharacters` that takes a string as parameter
+// and returns a list with the unique letters of the given string
+// Create basic unit tests for it with at least 3 different test cases
+// Should print out:
+// ["n", "g", "r", "m"]
+
